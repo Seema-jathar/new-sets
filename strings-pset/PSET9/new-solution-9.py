b5d164a6650a30d5221b6b47938332d26d178053
Milind <mjathar123@gmail.com>
2023-01-08 16:38:53 +0530

    Improve the solutuin-9

---
+++ strings-pset/PSET9/new-solution-9.py
@@ -0,0 +1,4 @@
+str_in = input("Please enter the string containing the letter a\n")
+#replacing the letter "a" with letter "b"
+new_str = str_in.replace('a','b')
+print("The new string after replacement\n",new_str)
